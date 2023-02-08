c2c7ec406ea59b182349791e7c5f6cb95bc20227
carolinainfante <angiecarolina.infante@gmail.com>
2023-02-08 13:07:01 -0500

    Update notepad_runner.java

--- src/test/java/net/onlinenotepad/runners/notepad_runner.java
+++ src/test/java/net/onlinenotepad/runners/notepad_runner.java
@@ -11,4 +11,15 @@ import org.junit.runner.RunWith;
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class notepad_runner {
+        
+        private WebDriver https://www.online-notepad.net/es/bloc-de-notas-onlinerowser;
+        private Actor nota = Actor.named("es una nota");
+
+public void setUp(){
+        nota.can(BrowseTheWeb.with)
+}     
+      
 }
+ 
+
+
